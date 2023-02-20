9899da17913f8da8329ea24813c47e270db57e45
mccameg <meganmccarren@yahoo.com>
2023-02-19 20:38:45 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+	 double meters = 0;
+	 double miles = 0;
+	 double feet = 0;
+	 double inches = 0;
+	 String trash = "";
+	 
+    }
+}
